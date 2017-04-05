c23d6511e42908d877ceead960a33742537e0103
kosiginiv83 <kosiginiv83gmail.com>
2017-04-05 18:01:26 +0800

    r6

--- Test2/Form1.cs
+++ Test2/Form1.cs
@@ -9,12 +9,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
 
 namespace Test2
 {
     public partial class Form1 : Form
     {
-        IWebDriver Browser;
+        public IWebDriver Browser;
 
         public Form1()
         {
@@ -23,19 +24,30 @@ namespace Test2
 
         private void button1_Click(object sender, EventArgs e) //Открыть
         {
+            //Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
             Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
             //Browser = new OpenQA.Selenium.Firefox.FirefoxDriver();
-            Browser.Navigate().GoToUrl("http://blog.csssr.ru/qa-engineer/");
-            
+            //Browser = new ChromeDriver();
+            //Browser.Navigate().GoToUrl("http://blog.csssr.ru/qa-engineer/");
 
-            //Browser.Navigate().GoToUrl("https://google.com");
+
+            Browser.Navigate().GoToUrl("https://google.com");
             //Browser.Manage().Window.Maximize();
         }
 
         private void button2_Click(object sender, EventArgs e) //Поехали
         {
+            IWebElement element;
+            element = Browser.FindElement(By.Id("gs_ok0"));
+            element.Click();
+            System.Threading.Thread.Sleep(3000);
+            element = Browser.FindElement(By.Id("K65")); element.Click();
+            element = Browser.FindElement(By.Id("K70")); element.Click();
+            element = Browser.FindElement(By.Id("K82")); element.Click();
+            //SendKeys.Send("ENTER");
+            element.SendKeys(OpenQA.Selenium.Keys.Enter);
 
-            
+
             //IWebElement[] el = new IWebElement[4];
             //IWebElement element;
             /*el[3] = Browser.FindElement(By.CssSelector("div.graphs-errors a"));
@@ -63,12 +75,12 @@ namespace Test2
 
         private void button3_Click(object sender, EventArgs e) //Выход
         {
-            //Application.Exit();
+            Browser.Quit();
+            Application.Exit();
 
-            IWebElement element;
             //element = Browser.FindElement(By.PartialLinkText("НАХОДИТЬ"));
-            element = Browser.FindElement(By.PartialLinkText("vk.com"));
-            element.Click();
+            //element = Browser.FindElement(By.PartialLinkText("vk.com"));
+            //element = Browser.FindElement(By.CssSelector("body > div.wrap > footer > nav > div:nth-child(2) > a"));
 
             /*
             element = Browser.FindElement(By.XPath("html/body/div[1]/section[1]/section/div[1]/a")); element.Click(); System.Threading.Thread.Sleep(1000);
@@ -111,10 +123,5 @@ namespace Test2
             element.Click();
             System.Threading.Thread.Sleep(1000);*/
         }
-
-        private static void NM(IWebElement element) {
-
-        }
-
     }
 }
